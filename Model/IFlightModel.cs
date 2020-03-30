bf816105e4fa2ce72acfbd0323ae2b6c47073ff5
avichai geldzaher <avichaig@gmail.com>
2020-03-30 16:05:08 +0300

    Added IFlightModel and FlightViewModel

---
+++ Model/IFlightModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorApp.Model
+{
+    interface IFlightModel
+    {
+        //connection to the simulator
+        void connect(string ip, int port);
+        void disconnect();
+        void start();
+        
+        
+    }
+}
