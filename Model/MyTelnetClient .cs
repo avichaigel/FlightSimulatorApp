b1faaae2057f2bb76a535d7ebd4da470e059b077
Ofir Netzer <ofir.netz@gmail.com>
2020-04-01 23:23:26 +0300

    Updated the Read method, now it is not relying on the string argument

--- Model/MyTelnetClient .cs
+++ Model/MyTelnetClient .cs
@@ -31,14 +31,24 @@ namespace FlightSimulatorApp.Model
 
         public string Read(string command)
         {
-            string official_command = "get " + command + "\n";
-            byte[] read = Encoding.ASCII.GetBytes(official_command);
-            client.GetStream().Write(read, 0, read.Length);
-            byte[] buffer = new byte[64];
-            client.GetStream().Read(buffer, 0, 64);
-            string data = Encoding.ASCII.GetString(buffer, 0, buffer.Length);
-            return data;
+            NetworkStream myNetworkStream = client.GetStream();
+            if (myNetworkStream.CanRead)
+            {
+                byte[] bufferReader = new byte[1024];
+                StringBuilder wholeMessage = new StringBuilder();
+                int readBytesNum = 0;
+                while (myNetworkStream.DataAvailable)
+                {
+                    readBytesNum = myNetworkStream.Read(bufferReader, 0, bufferReader.Length);
+                    wholeMessage.AppendFormat("{0}", Encoding.ASCII.GetString(bufferReader, 0, readBytesNum));
+                }
+                // print the message to the console
+                Console.WriteLine("Your Message: " + wholeMessage);
+                return wholeMessage.ToString();
+            }
+            return null;
         }
+
 
         public void Write(string command)
         {
