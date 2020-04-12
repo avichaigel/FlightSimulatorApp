55171c32e8537ab11d015cc3ab35727a02afc5f5
avichai geldzaher <avichaig@gmail.com>
2020-04-12 12:18:20 +0300

    added main viw model

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using FlightSimulatorApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorApp.ViewModel
+{
+    class MainViewModel
+    {
+        public DashboardViewModel DashBoardVM { get; set; }
+        public MapViewModel MapVM { get; set; }
+        public JoystickViewModel JoystickVM { get; set; }
+        public SlidersViewModel SlidersVM { get; set; }
+        public MainViewModel(IFlightModel flightModel)
+        {
+            flightModel = flightModel;
+            DashBoardVM = new DashboardViewModel(flightModel);
+            FlightControlsVM = new FlightControlsViewModel(flightModel);
+            MapVM = new MapViewModel(flightModel);
+
+        }
+    }
+}
